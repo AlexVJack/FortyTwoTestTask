f94c614613225a6186a81e28ea4a2ada5f531389
AlexVJack <alexvladjack@gmail.com>
2020-05-20 14:08:42 +0300

    ticket:1 Written tests for model and view that are failing

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,30 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from hello.models import PersonInfo
 
-# Create your tests here.
+
+class ViewTests(TestCase):
+    '''This covers template renders needed page'''
+    def test_about_using_template(self):
+        response = self.client.get(reverse('main'))
+        self.assertTemplateUsed(response,
+                                'hello/personinfo_list.html')
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_one_entry(self):
+        PersonInfo.objects.create(name='John')
+        response = self.client.get('/')
+        self.assertContains(response, 'John')
+
+
+class TestModels(TestCase):
+    '''This shows if model returns normal str'''
+    def test_model_str(self):
+        men = PersonInfo.objects.create(name="John")
+        self.assertEqual(str(men), "John")
 
 
 class SomeTests(TestCase):
