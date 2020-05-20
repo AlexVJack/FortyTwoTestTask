9fab6c318ea0f5499d4f7d6d0dc70135d125c205
AlexVJack <alexvladjack@gmail.com>
2020-05-20 14:40:08 +0300

    ticket:1 tests docstrings fix

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -4,25 +4,27 @@ from hello.models import PersonInfo
 
 
 class ViewTests(TestCase):
-    '''This covers template renders needed page'''
     def test_about_using_template(self):
+        "Tests that correct page is rendered"
         response = self.client.get(reverse('main'))
         self.assertTemplateUsed(response,
                                 'hello/personinfo_list.html')
 
     def test_homepage(self):
+        "Tests response code 200 of mainpage"
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_one_entry(self):
+    def test_one_field(self):
+        "Tests if field-name presented on response page"
         PersonInfo.objects.create(name='John')
         response = self.client.get('/')
         self.assertContains(response, 'John')
 
 
 class TestModels(TestCase):
-    '''This shows if model returns normal str'''
     def test_model_str(self):
+        "This shows if model returns normal str"
         men = PersonInfo.objects.create(name="John")
         self.assertEqual(str(men), "John")
 
