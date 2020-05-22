1302123d45c692f7a9ddb9bc5cd28b03abaa20dd
AlexVJack <alexvladjack@gmail.com>
2020-05-22 09:13:40 +0300

    ticket:3 added tests for model, view and middleware, test fails

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,35 +1,49 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-from hello.models import PersonInfo
+from mock import Mock
+from .models import HttpRequestModel, PersonInfo
+from .requests import CustomMiddleware
 
 
 class ViewTests(TestCase):
+    '''This covers template renders needed page'''
     def test_about_using_template(self):
-        "Tests that correct page is rendered"
         response = self.client.get(reverse('main'))
         self.assertTemplateUsed(response,
                                 'hello/personinfo_list.html')
+        response = self.client.get(reverse('requests'))
+        self.assertTemplateUsed(response,
+                                'hello/httprequestmodel_list.html')
 
     def test_homepage(self):
         "Tests response code 200 of mainpage"
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+        response = self.client.get('/requests/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_one_field(self):
+    def test_one_entry(self):
         "Tests if field-name presented on response page"
         PersonInfo.objects.create(name='John')
         response = self.client.get('/')
         self.assertContains(response, 'John')
+        HttpRequestModel.objects.create(method="GET")
+        response = self.client.get('/requests/')
+        self.assertContains(response, 'GET')
 
 
 class TestModels(TestCase):
     def test_model_str(self):
-        "This shows if model returns normal str"
+        "model returns str"
         men = PersonInfo.objects.create(name="John")
         self.assertEqual(str(men), "John")
 
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 4)
+class TestMiddleware(TestCase):
+    def test_requestProcessing(self):
+        "if middleware responds"
+        self.middleware = CustomMiddleware()
+        self.request = Mock()
+        self.request.session = {}
+        response = self.middleware.process_request(self.request)
+        self.assertIsNone(response)
