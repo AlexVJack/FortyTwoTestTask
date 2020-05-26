8358680a45006deb2e40bfe65b24f990bfdab683
AlexVJack <alexvladjack@gmail.com>
2020-05-26 15:42:56 +0300

    view added

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, UpdateView
 from .models import HttpRequestModel, PersonInfo
 
 
@@ -9,3 +9,9 @@ class PersonListView(ListView):
 class HttpRequestView(ListView):
     model = HttpRequestModel
     queryset = HttpRequestModel.objects.order_by('-time')[:10]
+
+
+class PersonUpdateView(UpdateView):
+    model = PersonInfo
+    fields = ['name', 'last_name', 'date_of_bearth', 'bio', 'email',
+              'jabber', 'skype', 'other_contacts']
