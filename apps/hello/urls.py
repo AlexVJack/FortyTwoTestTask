0c346c08fc95557d6e7102dfecb4e5d6ce2dd55a
AlexVJack <alexvladjack@gmail.com>
2020-05-26 15:50:04 +0300

    ticket:5 url fix

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import HttpRequestView, PersonListView
+from .views import HttpRequestView, PersonListView, PersonUpdateView
 
 
 urlpatterns = [
     url(r'^$', PersonListView.as_view(), name='main'),
     url(r'^requests/', HttpRequestView.as_view(), name='requests'),
+    url(r'^update/(?P<pk>\d+)/$', PersonUpdateView.as_view(), name='update'),
 ]
